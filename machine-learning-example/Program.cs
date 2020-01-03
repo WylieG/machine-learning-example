c339c61094c0c7b47ded5bd5e0cb213089233a0c
unknown <WylieG>
2020-01-03 16:18:18 +0000

    initial commit

---
+++ machine-learning-example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace machine_learning_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
